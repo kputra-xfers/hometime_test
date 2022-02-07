f07bfcd8b43b118c1fea229e7fb3284c1289f76c
Kharisma Putra <ks.dwiputra@gmail.com>
2022-02-07 17:25:50 +0800

    Add global error handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :show_errors
+
+  private
+
+  def show_errors(e)
+    Rails.logger.warn({ message: e.message, trace: e.backtrace })
+
+    if Rails.env.production? || Rails.env.test?
+      render json: { status: "Failed", message: e.message }, status: 422
+    else
+      raise e
+    end
+  end
 end
