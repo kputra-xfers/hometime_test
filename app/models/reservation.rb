1c0047ac714657b56f643300b50861804ff1bd83
Kharisma Putra <ks.dwiputra@gmail.com>
2022-02-08 11:59:33 +0800

    Add start date and end date validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,24 @@ class Reservation < ApplicationRecord
   belongs_to :guest
 
   validates :code, uniqueness: true
+  validate :start_date_is_today_or_after
+  validate :end_date_is_after_start_date
+
+  private
+
+  def end_date_is_after_start_date
+    return if end_date.blank? || start_date.blank?
+  
+    if end_date < start_date
+      errors.add(:end_date, "cannot be before the start date")
+    end 
+  end
+
+  def start_date_is_today_or_after
+    return if start_date.blank?
+
+    if start_date < Time.now
+      errors.add(:start_date, "cannot be before today")
+    end
+  end
 end
